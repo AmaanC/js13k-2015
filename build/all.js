5a72d96f685e734d5df853e5b85624b5d3006337
arshie <amaan.cheval@gmail.com>
2015-09-05 19:34:51 +0530

    Restructured

--- build/all.js
+++ build/all.js
@@ -1,39 +1,42 @@
 (function(exports) {
-    var polarity = function() {
-        return Math.random() > 0.5 ? 1 : -1;
-    };
+    // Hurray for self-contained functions!
+    exports.shakeScreen = (function() {    
+        var polarity = function() {
+            return Math.random() > 0.5 ? 1 : -1;
+        };
 
-    // Amount we've moved so far
-    var totalX = 0;
-    var totalY = 0;
+        // Amount we've moved so far
+        var totalX = 0;
+        var totalY = 0;
 
-    exports.shakeScreen = function(intensity) {
-        var ctx = exports.ctx;
-        if (totalX === 0) {
-            ctx.save();
-        }
-        if (!intensity) {
-            intensity = 2;
-        }
-        var dX = Math.random() * intensity * polarity();
-        var dY = Math.random() * intensity * polarity();
-        totalX += dX;
-        totalY += dY;
-
-        // Bring the screen back to its usual position every "2 intensity" so as not to get too far away from the center
-        if (intensity % 2 < 0.2) {
-            ctx.translate(-totalX, -totalY);
-            totalX = totalY = 0;
-            if (intensity <= 0.15) {
-                ctx.restore(); // Just to make sure it goes back to normal
-                return true;
+        return function(intensity) {
+            var ctx = exports.ctx;
+            if (totalX === 0) {
+                ctx.save();
             }
-        }
-        ctx.translate(dX, dY);
-        setTimeout(function() {
-            exports.shakeScreen(intensity - 0.1);
-        }, 5);
-    };
+            if (!intensity) {
+                intensity = 2;
+            }
+            var dX = Math.random() * intensity * polarity();
+            var dY = Math.random() * intensity * polarity();
+            totalX += dX;
+            totalY += dY;
+
+            // Bring the screen back to its usual position every "2 intensity" so as not to get too far away from the center
+            if (intensity % 2 < 0.2) {
+                ctx.translate(-totalX, -totalY);
+                totalX = totalY = 0;
+                if (intensity <= 0.15) {
+                    ctx.restore(); // Just to make sure it goes back to normal
+                    return true;
+                }
+            }
+            ctx.translate(dX, dY);
+            setTimeout(function() {
+                exports.shakeScreen(intensity - 0.1);
+            }, 5);
+        };
+    })();
 })(window.game);
 // (function(exports) {
     var exports = window.game;
