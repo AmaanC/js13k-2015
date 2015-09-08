adf03fa29c16cd7fe8444abe4a89b0dccd8e2544
Mike Buttery <email@mikedidthis.co.uk>
2015-09-08 23:44:23 +0100

    Added instrument settings. Added bass loop to audio.

--- src/js/audio.js
+++ src/js/audio.js
@@ -10,20 +10,59 @@
 
     // Tracks - Default
     tracks.default = {};
+
+    // TODO: Move to eighths to mirror drum loop.
     tracks.default.lead = [
-      'G3 q',
-      'E4 q',
-      'C4 h'
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
     ];
     tracks.default.harm = [
-      'G3 q',
-      'E4 q',
-      'C4 h'
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
+      '-   q',
     ];
     tracks.default.bass = [
-      'G3 q',
-      'E4 q',
-      'C4 h'
+      'A1 q',
+      '_  q',
+      'A1 q',
+      '_  q',
+      'A1 q',
+      'A2 q',
+      'A1 q',
+      '_  q',
+      'A1 q',
+      '_  q',
+      'A1 q',
+      '_  q',
+      'A1 q',
+      'A2 q',
+      'A1 q',
+      '_  q',
     ];
 
     // Create magic
@@ -32,24 +71,53 @@
     // Create audio
     var audio = {};
     exports.audio = audio;
-    audio.tempo = 90;
+
+    // Tempo and track
+    audio.tempo = 120;
     audio.track = tracks.default;
 
     // Give band tempo and music
-    audio.lead = new TinyMusic.Sequence( ac, audio.tempo, audio.track.lead );
-    audio.harm = new TinyMusic.Sequence( ac, audio.tempo, audio.track );
-    audio.bass = new TinyMusic.Sequence( ac, audio.tempo, audio.track );
+    var lead = new TinyMusic.Sequence( ac, audio.tempo, audio.track.lead );
+    var harm = new TinyMusic.Sequence( ac, audio.tempo, audio.track.harm );
+    var bass = new TinyMusic.Sequence( ac, audio.tempo, audio.track.bass );
+
+    exports.lead = lead;
+    exports.audio.harm = harm;
+    exports.audio.bass = bass;
+
+    // TODO: Make these easier to change, ala presets
+    lead.staccato = 0.55;
+    harm.staccato = 0;
+    bass.staccato = 0.5;
+
+    lead.gain.gain.value = 1.0;
+    harm.gain.gain.value = 0.2;
+    bass.gain.gain.value = 0.1;
+
+    lead.mid.frequency.value = 800;
+    lead.mid.gain.value = 3;
+    harm.mid.frequency.value = 1200;
+    bass.mid.gain.value = 3;
+    bass.bass.gain.value = -6;
+    bass.bass.frequency.value = 80;
+    bass.mid.gain.value = -6;
+    bass.mid.frequency.value = 100;
+    bass.treble.gain.value = -2;
+    bass.treble.frequency.value = 1400;
 
     exports.audioStart = function () {
-        audio.lead.play();
-        audio.harm.play();
-        audio.bass.play();
+        lead.play();
+        harm.play();
+        bass.play();
     };
 
     exports.audioStop = function () {
-        audio.lead.stop();
-        audio.harm.stop();
-        audio.bass.stop();
+        lead.stop();
+        harm.stop();
+        bass.stop();
     };
+
+    // Lazy...
+    exports.audioStart();
 
 })(window.game);
