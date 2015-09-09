495c238b2fff69681c888436c8577e87f914ab72
Amaan Cheval <amaan.cheval@gmail.com>
2015-09-09 19:54:36 +0530

    Fixed input problems on touch screens

--- src/js/input.js
+++ src/js/input.js
@@ -3,23 +3,24 @@
     exports.playerDirection = 1; // Becomes -1 when things should be reversed
 
     var inputPressed = function(direction) {
+        if (!exports.player.canMove) {
+            return;
+        }
         if (direction === 'left') {
-            exports.turnPlayer(exports.playerDirection);
+            exports.turnPlayer(-exports.playerDirection);
         }
         else if (direction === 'right') {
-            exports.turnPlayer(-exports.playerDirection);
+            exports.turnPlayer(exports.playerDirection);
         }
     };
 
     document.body.addEventListener('keydown', function(e) {
         exports.keys[e.keyCode] = true;
-        if (exports.player.canMove) {
-            if (e.keyCode === 39) {
-                inputPressed('left');
-            }
-            else if (e.keyCode === 37) {
-                inputPressed('right');
-            }
+        if (e.keyCode === 39) {
+            inputPressed('right');
+        }
+        else if (e.keyCode === 37) {
+            inputPressed('left');
         }
     });
     document.body.addEventListener('keyup', function(e) {
@@ -27,6 +28,7 @@
     });
 
     document.body.addEventListener('touchstart', function(e) {
+        console.log(e.changedTouches[0].pageX);
         if (e.changedTouches[0].pageX < exports.cx) {
             inputPressed('left');
         }
