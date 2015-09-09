8f7722e87d39ef210536b503c9e733716654abdf
Mike Buttery <email@mikedidthis.co.uk>
2015-09-10 00:16:48 +0100

    Added builds.

--- build/all.js
+++ build/all.js
@@ -218,125 +218,147 @@ Sequence.prototype.stop = function() {
   exports.Sequence = Sequence;
 }));
 
+// Tiny sfx with tinymusic!
+(function(exports) {
+
+    // Effects
+    var effects = {};
+    effects.default = [
+        'A1 e',
+    ];
+    effects.damage = [
+        'D1 q',
+    ];
+    exports.effects = effects;
+
+    // Create audio ctx, Tiny Music handles interaction
+    var ac = new AudioContext();
+
+    // SFX
+    var sfx = {};
+    exports.sfx = sfx;
+
+    sfx.tempo = 120;
+
+    // Cheap pattern changer
+    exports.sfxLoader = function(pattern) {
+
+        // Create instance for each effect as TinyNote can't change
+        // patterns using start / stop
+        var chan = new TinyMusic.Sequence( ac, sfx.tempo, pattern );
+        chan.loop = false;
+        chan.play();
+
+    };
+
+    // Tests
+    //exports.sfxLoader(effects.default);
+
+    //setTimeout(function() {
+        //exports.sfxLoader(effects.damage);
+    //}, 1000 );
+
+})(window.game);
+
 // Tiny audio with tinymusic!
 // Tracks are sheet music,
-// Audio contain the tempo, instruments ( + sheet music ).
-// Audio can be stop / started.
+// Nano emulates nanoloop app.
 (function(exports) {
 
     // Tracks
     var tracks = {};
-    exports.tracks = tracks;
-
     // Tracks - Default
     tracks.default = {};
+
+    // TODO: Move to eighths to mirror drum loop.
     tracks.default.lead = [
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
     ];
     tracks.default.harm = [
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
-      '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
+        '-   q',
     ];
     tracks.default.bass = [
-      'A1 q',
-      '_  q',
-      'A1 q',
-      '_  q',
-      'A1 q',
-      'A2 q',
-      'A1 q',
-      '_  q',
-      'A1 q',
-      '_  q',
-      'A1 q',
-      '_  q',
-      'A1 q',
-      'A2 q',
-      'A1 q',
-      '_  q',
+        'A1 q',
+        '_  q',
+        'A1 q',
+        '_  q',
+        'A1 q',
+        'A2 q',
+        'A1 q',
+        '_  q',
+        'A1 q',
+        '_  q',
+        'A1 q',
+        '_  q',
+        'A1 q',
+        'A2 q',
+        'A1 q',
+        '_  q',
     ];
 
-    // Create magic
+    // Create audio ctx, Tiny Music handles interaction
     var ac = new AudioContext();
 
-    // Create audio
-    var audio = {};
-    exports.audio = audio;
+    // Emulate nanoloops
+    var nano = {};
 
-    // Tempo and track
-    audio.tempo = 120;
-    audio.track = tracks.default;
+    // Export nano
+    exports.nano = nano;
 
-    // Give band tempo and music
-    var lead = new TinyMusic.Sequence( ac, audio.tempo, audio.track.lead );
-    var harm = new TinyMusic.Sequence( ac, audio.tempo, audio.track.harm );
-    var bass = new TinyMusic.Sequence( ac, audio.tempo, audio.track.bass );
+    // Set tempo and patten
+    nano.tempo = 120;
+    nano.pattern = tracks.default;
 
-    exports.lead = lead;
-    exports.audio.harm = harm;
-    exports.audio.bass = bass;
+    // Create nanoloops channels
+    var chanA = new TinyMusic.Sequence( ac, nano.temp, nano.pattern.lead );
+    var chanB = new TinyMusic.Sequence( ac, nano.temp, nano.pattern.bass );
+    var chanC = new TinyMusic.Sequence( ac, nano.temp, nano.pattern.lead );
+    var chanD = new TinyMusic.Sequence( ac, nano.temp, nano.pattern.lead );
 
-    // TODO: Make these easier to change, ala presets
-    lead.staccato = 0.55;
-    harm.staccato = 0;
-    bass.staccato = 0.5;
-
-    lead.gain.gain.value = 1.0;
-    harm.gain.gain.value = 0.2;
-    bass.gain.gain.value = 0.1;
-
-    lead.mid.frequency.value = 800;
-    lead.mid.gain.value = 3;
-    harm.mid.frequency.value = 1200;
-    bass.mid.gain.value = 3;
-    bass.bass.gain.value = -6;
-    bass.bass.frequency.value = 80;
-    bass.mid.gain.value = -6;
-    bass.mid.frequency.value = 100;
-    bass.treble.gain.value = -2;
-    bass.treble.frequency.value = 1400;
-
-    exports.audioStart = function () {
-        lead.play();
-        harm.play();
-        bass.play();
+    exports.nanoStart = function () {
+        chanA.play();
+        chanB.play();
+        chanC.play();
+        chanD.play();
     };
 
-    exports.audioStop = function () {
-        lead.stop();
-        harm.stop();
-        bass.stop();
+    exports.nanoStop = function () {
+        chanA.stop();
+        chanB.stop();
+        chanC.stop();
+        chanD.stop();
     };
 
-    // Lazy...
-    exports.audioStart();
+    // Tests.
+    //exports.nanoStart();
 
 })(window.game);
 
