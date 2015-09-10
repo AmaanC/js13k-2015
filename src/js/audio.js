34bcee1679fec712f8730d25381d87bc17accdc5
Mike Buttery <email@mikedidthis.co.uk>
2015-09-10 21:58:48 +0100

    Reworked audio to play 12 channels / tracks.

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,101 +1,89 @@
 // Tiny audio with tinymusic!
 // Tracks are sheet music,
-// Nano emulates nanoloop app.
+// Audio emulates nanoloop app.
 (function(exports) {
-
-    // Tracks
-    var tracks = {};
-    // Tracks - Default
-    tracks.default = {};
-
-    // TODO: Move to eighths to mirror drum loop.
-    tracks.default.lead = [
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-    ];
-    tracks.default.harm = [
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-        '-   q',
-    ];
-    tracks.default.bass = [
-        'A1 q',
-        '_  q',
-        'A1 q',
-        '_  q',
-        'A1 q',
-        'A2 q',
-        'A1 q',
-        '_  q',
-        'A1 q',
-        '_  q',
-        'A1 q',
-        '_  q',
-        'A1 q',
-        'A2 q',
-        'A1 q',
-        '_  q',
-    ];
 
     // Create audio ctx, Tiny Music handles interaction
     var ac = new AudioContext();
 
     // Emulate nanoloops
-    var nano = {};
+    var audio = exports.audio = {};
 
-    // Export nano
-    exports.nano = nano;
+    // Set tempo and track count
+    audio.tempo = 120;
+    audio.trackCount = 12;
 
-    // Set tempo and patten
-    nano.tempo = 120;
-    nano.pattern = tracks.default;
+    // Patterns - 4 x 16 bar.
+    // w  - 1
+    // h  - 1/2
+    // q  - 1/4
+    // e  - 1/8
+    // s  - 1/16
+    // es - 1/8 + 1/16
+    var patterns = {};
 
-    // Create nanoloops channels
-    var chanA = new TinyMusic.Sequence( ac, nano.temp, nano.pattern.lead );
-    var chanB = new TinyMusic.Sequence( ac, nano.temp, nano.pattern.bass );
-    var chanC = new TinyMusic.Sequence( ac, nano.temp, nano.pattern.lead );
-    var chanD = new TinyMusic.Sequence( ac, nano.temp, nano.pattern.lead );
+    patterns[0] = [
+        'A1 q',
+        '_  q',
+        'A1 q',
+        '_  q',
+        'A1 q',
+        'A2 q',
+        'A1 q',
+        '_  q',
+        'A1 q',
+        '_  q',
+        'A1 q',
+        '_  q',
+        'A1 q',
+        'A2 q',
+        'A1 q',
+        '_  q',
+    ];
 
-    exports.nanoStart = function () {
-        chanA.play();
-        chanB.play();
-        chanC.play();
-        chanD.play();
+    // Create tracks
+    var tracks = [];
+    for (var i = 0; i < audio.trackCount; i++) {
+
+        var track = new TinyMusic.Sequence( ac, audio.tempo, patterns[i] );
+        track.loop = true;
+        tracks.push( track );
+
+    }
+
+    // Configure instruments
+    // Smoothing
+    tracks[0].smoothing = 0;
+    tracks[0].staccato = 0;
+
+    // Volume
+    tracks[0].gain.gain.value = 1;
+
+    // Mid
+    tracks[0].mid.frequency.value = 800;
+    tracks[0].mid.gain.value = 3;
+
+    // Bass
+    tracks[0].bass.gain.value = 6;
+    tracks[0].bass.frequency.value = 80;
+
+    // Trebile
+    tracks[0].treble.gain.value = -2;
+    tracks[0].treble.frequency.value = 1400;
+
+
+    // Start tracks
+    exports.startAudio = function() {
+        for (var i = 0; i < tracks.length; i++) {
+            tracks[i].play();
+        }
     };
 
-    exports.nanoStop = function () {
-        chanA.stop();
-        chanB.stop();
-        chanC.stop();
-        chanD.stop();
+    // Stop tracks
+    exports.startStop = function() {
+        for (var i = 0; i < tracks.length; i++) {
+            tracks[i].stop();
+        }
     };
-
-    // Tests.
-    //exports.nanoStart();
 
 })(window.game);
