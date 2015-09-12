5f6505c975b76458675575970021cedf5c907256
Mike Buttery <email@mikedidthis.co.uk>
2015-09-12 11:11:03 +0100

    Builds

--- build/all.js
+++ build/all.js
@@ -1,171 +1,3 @@
-(function(exports) {
-    var canvas = exports.canvas = document.getElementById('game');
-    var ctx = exports.ctx = canvas.getContext('2d');
-    if (window.innerWidth < canvas.width) {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        document.body.style.margin = 0;
-    }
-    exports.smallerDimension = (canvas.width < canvas.height) ? canvas.width : canvas.height;
-    exports.cx = canvas.width / 2;
-    exports.cy = canvas.height / 2;
-    exports.player = {};
-
-    var drawLoop = function() {
-        exports.backgroundDraw();
-        exports.playerDraw();
-        exports.particleDraw();
-        exports.enemyDraw();
-
-        if (exports.currentState === 'endScreen') {
-            exports.endScreenDraw();
-        }
-
-        requestAnimationFrame(drawLoop);
-    };
-
-    var logicLoop = function() {
-        exports.backgroundLogic();
-        exports.particleLogic();
-        exports.enemyLogic();
-
-        setTimeout(logicLoop, 100 / 6);
-    };
-
-    var init = function() {
-        drawLoop();
-        logicLoop();
-    };
-
-    window.addEventListener('load', init, false);
-})(window.game);
-(function(exports) {
-    var ctx = exports.ctx;
-
-    var player = exports.player;
-    player.cx = exports.cx;
-    player.cy = exports.cy;
-    player.time = 0;
-    player.dist = 40;
-    player.halfBase = 10;
-    player.halfHeight = 10;
-    player.angle = 0;
-    player.canMove = true;
-    player.pos = 0; // This indicates which multiple of turnStep it is. For example, with 4 sides, player would point down when pos is 1
-    player.alpha = 1;
-    player.DEFAULT_NUM_SHIELDS = 1;
-    player.numShields = player.DEFAULT_NUM_SHIELDS; // Shields are automatically drawn with the player
-    player.score = 0;
-    var DIST_BETWEEN_SHIELDS = 20;
-    var shieldMinDist = player.dist + 2 * player.halfHeight;
-    var SHIELD_COLOR = 'white';
-    var SHIELD_RANGE = 0.4;
-
-
-    player.hidePlayer = function() {
-        player.alpha = 0;
-        setTimeout(function() {
-            player.color = player.skins.default;
-        }, 1000);
-    };
-
-    player.isColliding = function(objCenterDist) {
-        // Only in terms of distance, not in position
-        if (
-            (player.numShields <= 0 && objCenterDist <= player.dist) ||
-            (player.numShields > 0 && objCenterDist <= player.numShields * DIST_BETWEEN_SHIELDS + shieldMinDist)
-        ) {
-            return true;
-        }
-        return false;
-    };
-
-    var shieldDraw = function() {
-        ctx.strokeStyle = SHIELD_COLOR;
-        for (var i = 1; i <= player.numShields; i++) {
-            ctx.beginPath();
-            ctx.arc(0, 0, shieldMinDist + i * DIST_BETWEEN_SHIELDS, -SHIELD_RANGE, SHIELD_RANGE, false);
-            ctx.closePath();
-            ctx.stroke();
-        }
-    };
-
-    exports.playerDraw = function() {
-        var cx = player.cx;
-        var cy = player.cy;
-        var hb = player.halfBase;
-        var hh = player.halfHeight;
-        ctx.save();
-
-        ctx.translate(cx, cy);
-        ctx.rotate(player.angle);
-        shieldDraw();
-
-        // The triangle points to the right by default
-        ctx.translate(player.dist + hh, 0);
-
-        // Draw path
-        ctx.beginPath();
-        ctx.moveTo(-hh, -hb);
-        ctx.lineTo(-hh, hb);
-        ctx.lineTo(hh, 0);
-        ctx.closePath();
-        ctx.restore();
-
-        // Stroke
-        ctx.lineWidth = 5;
-        ctx.lineJoin = 'round';
-        ctx.strokeStyle = 'rgba('+player.color+', ' + player.alpha + ')';
-        ctx.stroke();
-
-        // Fill
-        ctx.fillStyle = 'rgba('+player.color+', 0)';
-        ctx.fill();
-
-
-    };
-
-    exports.turnPlayer = function(dir) {
-        player.pos += dir;
-        if (player.pos >= exports.sides) {
-            player.pos %= exports.sides;
-        }
-        if (player.pos < 0) {
-            player.pos = exports.sides + player.pos;
-        }
-        player.angle = exports.turnStep * player.pos;
-        player.restAngle = player.angle;
-    };
-
-    exports.setPlayerDirection = function(dir) {
-        exports.playerDirection = dir;
-        if (exports.playerDirection > 0) {
-            exports.ctx.globalCompositeOperation = 'source-over';
-        }
-        else {
-            exports.ctx.globalCompositeOperation = 'difference';
-        }
-    };
-
-})(window.game);
-
-(function(exports) {
-    // Used in player.js
-    exports.player.skins = {
-        default: '13, 213, 252',
-        flashColor: '255, 184, 253' // The color it flashes briefly when hit
-    };
-    exports.player.color = exports.player.skins.default;
-
-    // Used in gameHandler.js
-    exports.HIT_PARTICLE_COLORS = ['red']; // The color of the particles emitted when the player's triangle is crushed
-    exports.NORMAL_ENEMY_COLOR = 'white';
-    exports.REVERSER_ENEMY_COLOR = 'green';
-
-    // Used in background.js
-    exports.DEFAULT_BACKGROUND_COLORS = ['#BF0C43', '#F9BA15', '#8EAC00', '#127A97', '#452B72'];
-    exports.INDICATOR_COLOR = 'white';
-})(window.game);
 (function ( root, factory ) {
   if ( typeof define === 'function' && define.amd ) {
     define( [ 'exports' ], factory );
@@ -670,6 +502,47 @@ Sequence.prototype.stop = function() {
 })(window.game);
 
 (function(exports) {
+    var canvas = exports.canvas = document.getElementById('game');
+    var ctx = exports.ctx = canvas.getContext('2d');
+    if (window.innerWidth < canvas.width) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        document.body.style.margin = 0;
+    }
+    exports.smallerDimension = (canvas.width < canvas.height) ? canvas.width : canvas.height;
+    exports.cx = canvas.width / 2;
+    exports.cy = canvas.height / 2;
+    exports.player = {};
+
+    var drawLoop = function() {
+        exports.backgroundDraw();
+        exports.playerDraw();
+        exports.particleDraw();
+        exports.enemyDraw();
+
+        if (exports.currentState === 'endScreen') {
+            exports.endScreenDraw();
+        }
+
+        requestAnimationFrame(drawLoop);
+    };
+
+    var logicLoop = function() {
+        exports.backgroundLogic();
+        exports.particleLogic();
+        exports.enemyLogic();
+
+        setTimeout(logicLoop, 100 / 6);
+    };
+
+    var init = function() {
+        drawLoop();
+        logicLoop();
+    };
+
+    window.addEventListener('load', init, false);
+})(window.game);
+(function(exports) {
     exports.keys = {};
     exports.playerDirection = 1; // Becomes -1 when things should be reversed
 
@@ -708,6 +581,116 @@ Sequence.prototype.stop = function() {
         }
     });
 })(window.game);
+(function(exports) {
+    var ctx = exports.ctx;
+
+    var player = exports.player;
+    player.cx = exports.cx;
+    player.cy = exports.cy;
+    player.time = 0;
+    player.dist = 40;
+    player.halfBase = 10;
+    player.halfHeight = 10;
+    player.angle = 0;
+    player.canMove = true;
+    player.pos = 0; // This indicates which multiple of turnStep it is. For example, with 4 sides, player would point down when pos is 1
+    player.alpha = 1;
+    player.DEFAULT_NUM_SHIELDS = 1;
+    player.numShields = player.DEFAULT_NUM_SHIELDS; // Shields are automatically drawn with the player
+    player.score = 0;
+    var DIST_BETWEEN_SHIELDS = 20;
+    var shieldMinDist = player.dist + 2 * player.halfHeight;
+    var SHIELD_COLOR = 'white';
+    var SHIELD_RANGE = 0.4;
+
+
+    player.hidePlayer = function() {
+        player.alpha = 0;
+        setTimeout(function() {
+            player.color = player.skins.default;
+        }, 1000);
+    };
+
+    player.isColliding = function(objCenterDist) {
+        // Only in terms of distance, not in position
+        if (
+            (player.numShields <= 0 && objCenterDist <= player.dist) ||
+            (player.numShields > 0 && objCenterDist <= player.numShields * DIST_BETWEEN_SHIELDS + shieldMinDist)
+        ) {
+            return true;
+        }
+        return false;
+    };
+
+    var shieldDraw = function() {
+        ctx.strokeStyle = SHIELD_COLOR;
+        for (var i = 1; i <= player.numShields; i++) {
+            ctx.beginPath();
+            ctx.arc(0, 0, shieldMinDist + i * DIST_BETWEEN_SHIELDS, -SHIELD_RANGE, SHIELD_RANGE, false);
+            ctx.closePath();
+            ctx.stroke();
+        }
+    };
+
+    exports.playerDraw = function() {
+        var cx = player.cx;
+        var cy = player.cy;
+        var hb = player.halfBase;
+        var hh = player.halfHeight;
+        ctx.save();
+
+        ctx.translate(cx, cy);
+        ctx.rotate(player.angle);
+        shieldDraw();
+
+        // The triangle points to the right by default
+        ctx.translate(player.dist + hh, 0);
+
+        // Draw path
+        ctx.beginPath();
+        ctx.moveTo(-hh, -hb);
+        ctx.lineTo(-hh, hb);
+        ctx.lineTo(hh, 0);
+        ctx.closePath();
+        ctx.restore();
+
+        // Stroke
+        ctx.lineWidth = 5;
+        ctx.lineJoin = 'round';
+        ctx.strokeStyle = 'rgba('+player.color+', ' + player.alpha + ')';
+        ctx.stroke();
+
+        // Fill
+        ctx.fillStyle = 'rgba('+player.color+', 0)';
+        ctx.fill();
+
+
+    };
+
+    exports.turnPlayer = function(dir) {
+        player.pos += dir;
+        if (player.pos >= exports.sides) {
+            player.pos %= exports.sides;
+        }
+        if (player.pos < 0) {
+            player.pos = exports.sides + player.pos;
+        }
+        player.angle = exports.turnStep * player.pos;
+        player.restAngle = player.angle;
+    };
+
+    exports.setPlayerDirection = function(dir) {
+        exports.playerDirection = dir;
+        if (exports.playerDirection > 0) {
+            exports.ctx.globalCompositeOperation = 'source-over';
+        }
+        else {
+            exports.ctx.globalCompositeOperation = 'difference';
+        }
+    };
+
+})(window.game);
+
 // The game has "stages", which are represented by different shapes, i.e. the square is one stage, and then the pentagon is the next.
 // Within each stage, there are the following levels:
 // 1) Faster enemies
