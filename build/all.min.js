16899775fc49866ee10ea4b61acc8c6aefccc041
Amaan Cheval <amaan.cheval@gmail.com>
2015-09-12 17:30:51 +0530

    Fades text in on end screen too

--- build/all.min.js
+++ build/all.min.js
@@ -1613,12 +1613,12 @@ Sequence.prototype.stop = function() {
     var canvas = exports.canvas;
     var alpha = 0;
     exports.endScreenDraw = function() {
-        ctx.fillStyle = 'rgba(0, 0, 0, ' + alpha + ')'
+        ctx.fillStyle = 'rgba(0, 0, 0, ' + alpha + ')';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         alpha += 0.01;
+        exports.write('Score: ' + exports.player.score, 'center', 'center', 5, 'rgba(255, 255, 255, ' + 2 * alpha + ')');
         if (alpha >= 0.5) {
             alpha = 0.5;
-            exports.write('Score: ' + exports.player.score, 'center', 'center', 5, 'white');
         }
     };
 })(window.game);
