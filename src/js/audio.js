d763df030705d1f494aa72e4c00ea9e04e9760f8
Mike Buttery <email@mikedidthis.co.uk>
2015-09-12 11:09:46 +0100

    Audio can now be changed externally.

--- src/js/audio.js
+++ src/js/audio.js
@@ -3,6 +3,23 @@
 // Audio emulates nanoloop app.
 (function(exports) {
 
+    // Change Octave
+    var changeOctave = function(originalArray, changeBy) {
+        var ret = [];
+        var num = 0;
+        var newNote;
+        ret = originalArray.map(function(note) {
+            newNote = note;
+            num = Number(note.match(/\d/));
+            return note.replace(num, num + changeBy);
+        });
+        return ret;
+    };
+
+    var changePreset = function(seq, preset) {
+
+    };
+
     // Create audio ctx, Tiny Music handles interaction
     var ac = typeof AudioContext !== 'undefined' ? new AudioContext : new webkitAudioContext;
 
@@ -19,24 +36,11 @@
     // e  - 1/8
     // s  - 1/16
     // es - 1/8 + 1/16
-
-
-    var changeOctave = function(originalArray, changeBy) {
-        var ret = [];
-        var num = 0;
-        var newNote;
-        ret = originalArray.map(function(note) {
-            newNote = note;
-            num = Number(note.match(/\d/));
-            return note.replace(num, num + changeBy);
-        });
-        return ret;
-    };
+    var patterns = [];
 
     // Bass
-    var bass = [];
-
-    bass[0] = [
+    patterns[0] = [];
+    patterns[0][0] = [
         'E1  e',
         'F1  e',
         'F1  e',
@@ -73,7 +77,7 @@
         'B1  e',
         'B1  e'
     ];
-    bass[1] = [
+    patterns[0][1] = [
         'E1  e',
         'F1  e',
         'F1  e',
@@ -112,8 +116,8 @@
     ];
 
     // Harmony
-    var harmony = [];
-    harmony[0] = [
+    patterns[1]= [];
+    patterns[1][0] = [
         'G1  w',
 
         'Bb1 w',
@@ -136,11 +140,11 @@
         'B1  e',
         'A1  e'
     ];
-    harmony[1] = changeOctave(harmony[0], 1);
+    patterns[1][1] = changeOctave(patterns[1][0], 1);
 
     // Lead.
-    var lead = [];
-    lead[0] = [
+    patterns[2] = [];
+    patterns[2][0] = [
         '-   w',
 
         '-   w',
@@ -188,38 +192,63 @@
         'D2  e',
         'D2  e'
     ];
-
-    lead[1] = [
-    ];
-
-
+    patterns[2][1] = changeOctave(patterns[2][0], 1);
 
     // Create sequences
-    var seq1 = new TinyMusic.Sequence( ac, audio.tempo, bass[0] );
-    var seq2 = new TinyMusic.Sequence( ac, audio.tempo, harmony[0] );
-    var seq3 = new TinyMusic.Sequence( ac, audio.tempo, lead[0] );
+    var seqs = [];
+    seqs[0] = new TinyMusic.Sequence(ac, audio.tempo, []);
+    seqs[1] = new TinyMusic.Sequence(ac, audio.tempo, []);
+    seqs[2] = new TinyMusic.Sequence(ac, audio.tempo, []);
 
     // Set volume
-    seq1.gain.gain.value = 0.6;
-    seq2.gain.gain.value = 0.8;
-    seq3.gain.gain.value = 0.8;
+    seqs[0].gain.gain.value = 0.6;
+    seqs[1].gain.gain.value = 0.8;
+    seqs[2].gain.gain.value = 0.8;
 
     // Set coolness
-    seq1.smoothing = 0.5;
-    seq2.smoothing = 0.1;
-    seq3.smoothing = 0.1;
-    seq1.staccato = 0.4;
-    seq2.staccato = 0.4;
-    seq3.staccato = 0.4;
+    seqs[0].smoothing = 0.5;
+    seqs[1].smoothing = 0.1;
+    seqs[2].smoothing = 0.1;
+    seqs[0].staccato = 0.4;
+    seqs[1].staccato = 0.4;
+    seqs[2].staccato = 0.4;
 
     // Set wave
-    seq1.createCustomWave([-1,0,1,0.5,-1,0,1]);
-    seq2.createCustomWave([-1,1,-1,1,-1,1],[1,0,1,0,1,0]);
-    seq3.createCustomWave([-1,-0.9,-0.6,-0.3, 0, 0.3, 0.6, 0.9,1]);
+    seqs[0].createCustomWave([-1,0,1,0.5,-1,0,1]);
+    seqs[1].createCustomWave([-1,1,-1,1,-1,1],[1,0,1,0,1,0]);
+    seqs[2].createCustomWave([-1,-0.9,-0.6,-0.3, 0, 0.3, 0.6, 0.9,1]);
 
-    console.log( lead, seq1, seq2, seq3 );
-    seq1.play();
-    seq2.play();
-    seq3.play();
+    // Start all sequence
+    exports.audioStart = function() {
+        seqs[0].play();
+        seqs[1].play();
+        seqs[2].play();
+    };
+
+    exports.audioAddPreset = function(seq,preset) {
+
+    };
+
+    // Adds loop to seq
+    exports.audioAddLoop = function(seq,pattern,loop) {
+        seqs[seq].push.apply(seqs[seq], patterns[pattern][loop]);
+    };
+
+    exports.audioStart();
+
+    // Add loop one.
+    exports.audioAddLoop(0,0,0);
+    exports.audioAddLoop(1,1,0);
+    exports.audioAddLoop(2,2,0);
+
+    // Add loop two.
+    exports.audioAddLoop(0,0,1);
+    exports.audioAddLoop(1,1,1);
+    exports.audioAddLoop(2,2,1);
+
+    // Mess with all the things
+    exports.audioAddLoop(0,2,1);
+    exports.audioAddLoop(1,0,1);
+    exports.audioAddLoop(2,1,0);
 
 })(window.game);
