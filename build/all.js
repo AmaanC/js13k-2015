46c9ce04117918707f7b8276593e38e041acb75e
Mike Buttery <email@mikedidthis.co.uk>
2015-09-13 00:43:20 +0100

    Build :art:

--- build/all.js
+++ build/all.js
@@ -119,16 +119,9 @@
         ctx.closePath();
         ctx.restore();
 
-        // Stroke
-        ctx.lineWidth = 5;
-        ctx.lineJoin = 'round';
-        ctx.strokeStyle = 'rgba('+player.color+', ' + player.alpha + ')';
-        ctx.stroke();
-
         // Fill
-        ctx.fillStyle = 'rgba('+player.color+', 0)';
+        ctx.fillStyle = 'rgba('+player.color+', 1)';
         ctx.fill();
-
 
     };
 
@@ -157,25 +150,86 @@
 })(window.game);
 
 (function(exports) {
+
+    // Shamelessly borrowed - http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+    var shadeRGBColor = function(color, percent) {
+        var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
+        return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
+    }
+    var shadeColor2 = function(color, percent) {
+        var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+        return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+    }
+    var shade = exports.shade = function(color, percent){
+        if (color.length > 7 ) return shadeRGBColor(color,percent);
+        else return shadeColor2(color,percent);
+    }
+    var shuffleArray = function(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
     // Used in player.js
+    // Shield needs to be 0,225,172
     exports.player.skins = {
-        default: '13, 213, 252',
+        default: '255,255,255',
         flashColor: '255, 184, 253' // The color it flashes briefly when hit
     };
     exports.player.color = exports.player.skins.default;
 
+    // Used in background.js
+    exports.bgIndex = 0;
+    exports.BACKGROUND_COLORS_LIST = shuffleArray([
+        ['#272D4D'],
+        ['#B83564'],
+        ['#FF6A5A'],
+        ['#FFB350'],
+        ['#83B8AA'],
+
+        ['#490A3D'],
+        ['#BD1550'],
+        ['#E97F02'],
+        ['#F8CA00'],
+        ['#8F293D'],
+
+        ['#C75233'],
+        ['#C78933'],
+        ['#D6CEAA'],
+        ['#79B5AC'],
+        ['#5E2F46'],
+
+        ['#FF003C'],
+        ['#FF8A00'],
+        ['#FABE28'],
+        ['#88C100'],
+        ['#00C176'],
+
+        ['#FAF577'],
+        ['#67DAD4'],
+        ['#24AFA8'],
+        ['#28716D'],
+        ['#EF434B'],
+
+        ['#482344'],
+        ['#429867'],
+        ['#2B5166'],
+        ['#FAB243'],
+        ['#E02130'],
+
+        // emo mode
+        ['#212121'],
+        ['#efefef']
+    ]);
+    exports.INDICATOR_COLOR = '#1B9171';
+
     // Used in gameHandler.js
     exports.HIT_PARTICLE_COLORS = ['red', 'white']; // The color of the particles emitted when the player's triangle is crushed
     exports.NORMAL_ENEMY_COLOR = 'white';
-    exports.REVERSER_ENEMY_COLOR = 'green';
-
-    // Used in background.js
-    exports.bgIndex = 0;
-    exports.BACKGROUND_COLORS_LIST = [
-        ['#BF0C43', '#F9BA15', '#8EAC00', '#127A97', '#452B72'],
-        ['#BF0C43', '#F9BA15', '#452B72', '#127A97', '#8EAC00']
-    ];
-    exports.INDICATOR_COLOR = 'white';
+    exports.REVERSER_ENEMY_COLOR = '#FD0251';
 
     // Used in uiScreens.js
     exports.MAIN_OVERLAY_RGBA = '0, 0, 0, 0.5';
@@ -184,8 +238,9 @@
     exports.END_TEXT_COLOR = '255, 255, 255';
     exports.MUSIC_ENABLED_COLOR = 'white';
     exports.MUSIC_DISABLED_COLOR = 'gray';
-    
+
 })(window.game);
+
 (function ( root, factory ) {
   if ( typeof define === 'function' && define.amd ) {
     define( [ 'exports' ], factory );
@@ -1776,7 +1831,8 @@ Sequence.prototype.stop = function() {
 
     exports.changeColors = function(arr) {
         for (var i = exports.NUM_SHAPES - 1; i >= 0; i--) {
-            shapes[i].color = arr[i % arr.length];
+            shapes[i].color = exports.shade(arr[0], -0.1*i/2);
+            exports.INDICATOR_COLOR = arr[0];
         }
     };
 
@@ -1784,7 +1840,7 @@ Sequence.prototype.stop = function() {
         var obj;
         exports.steps = step;
         exports.spinning = true;
-        
+
         for (var i = 0; i < shapes.length; i++) {
             obj = shapes[i];
             obj.spinning = true;
@@ -1818,6 +1874,7 @@ Sequence.prototype.stop = function() {
     exports.initBackground();
 
 })(window.game);
+
 (function(exports) {
     var ctx = exports.ctx;
     var canvas = exports.canvas;
